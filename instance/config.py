0022f3739f0fc148c0d1a09efe75ef4fad7c0929
BhanuKondi <bhanu@atikes.com>
2026-06-03 15:43:13 +0530

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,8 +1,7 @@
 # Defaults are intentionally non-sensitive.
 # Real values should come from environment variables or .env.local.
 MYSQL_HOST = "localhost"
-MYSQL_PORT = 3306
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "bhanu"
-MYSQL_DATABASE = "hr_application"
-SECRET_KEY = "change-me"
+MYSQL_USER = "hruser"
+MYSQL_PASSWORD = "Hr@1234"
+MYSQL_DATABASE = "hrapp1"
+SECRET_KEY = "xyz"
